cd9f64bdb781d1c562902e2cd0fe640db631d32d
Tim Ng <timng@tims-mbp.mynet>
2018-12-25 00:39:20 +0800

    Initial commit

---
+++ ConcealAPIKeyTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcealAPIKeyTest
+//
+//  Created by Tim Ng on 12/25/18.
+//  Copyright © 2018 Tim Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
