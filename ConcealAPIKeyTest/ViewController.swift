ccf78d3f6fe54ed7c5f98aec8e66e4a89c1fe688
Tim Ng <timng@tims-mbp.mynet>
2018-12-25 00:49:31 +0800

    API Key testing

--- ConcealAPIKeyTest/ViewController.swift
+++ ConcealAPIKeyTest/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import Keys
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let keys = ConcealAPIKeyTestKeys()
+        print("\(keys.theySaidSoAPIKey)")
     }
 
 
